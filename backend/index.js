88c8ecb3cfd3f871841f632f1f73866a235bdd95
Ralf Cueva <ralfcueva3@gmail.com>
2026-09-24 22:27:28 +0530

    Added rateLimiter to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
-require("dotenv").config();          // loads .env
+require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const connectDB = require("./config/db");  // ← db
+const connectDB = require("./config/db");  
 
-const userRoutes  = require("./routes/user.js");   // ← routes
-const adminRoutes = require("./routes/Admin");  // ← routes
-const aiRoutes    = require("./routes/ai");     // ← routes
+const userRoutes  = require("./routes/user.js");
+const adminRoutes = require("./routes/Admin");
+const aiRoutes    = require("./routes/Ai");     
+
+const rateLimiter = require("./middleware/rateLimiter.js")
+app.use(ratelimiter)
 
 const app = express();
-connectDB();                          // ← runs db connection
+connectDB();                          
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/user", userRoutes);    // ← mounts routes
+app.use("/api/user", userRoutes);    
 app.use("/api/admin", adminRoutes);
 app.use("/api/ai", aiRoutes);
+
+
 
 app.listen(process.env.PORT || 3000, () =>
   console.log("Server running on port 3000")
